64b20d4be1e87fd10a75eda4fecd158948a224ae
Sasu Korhonen <smage90@hotmail.com>
2022-02-28 08:54:37 +0200

    fix: fixed project structure

---
+++ src/test/java/kornas/moviecharactersapi/MovieCharactersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kornas.moviecharactersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCharactersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
