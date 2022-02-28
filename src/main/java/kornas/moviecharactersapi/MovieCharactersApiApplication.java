2299e6eaa0e665c5244421aeac67ffc318e723aa
Sasu Korhonen <smage90@hotmail.com>
2022-02-28 15:33:45 +0200

    feat: Dockerfile and compose + removed one git folder.

--- src/main/java/kornas/moviecharactersapi/MovieCharactersApiApplication.java
+++ src/main/java/kornas/moviecharactersapi/MovieCharactersApiApplication.java
@@ -2,8 +2,9 @@ package kornas.moviecharactersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MovieCharactersApiApplication {
 
 	public static void main(String[] args) {
