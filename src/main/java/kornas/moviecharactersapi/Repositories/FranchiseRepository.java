7e72921e90f453c6a6c812a33c4e0127da177fa2
Sasu Korhonen <smage90@hotmail.com>
2022-02-28 16:19:54 +0200

    feat: Added few annotations to controllers and repositories

--- src/main/java/kornas/moviecharactersapi/Repositories/FranchiseRepository.java
+++ src/main/java/kornas/moviecharactersapi/Repositories/FranchiseRepository.java
@@ -4,6 +4,7 @@ import kornas.moviecharactersapi.Models.Franchise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FranchiseRepository extends JpaRepository<Franchise, Integer> {
 
 }
