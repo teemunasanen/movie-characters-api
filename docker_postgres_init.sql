d6273dea92c624d9f4efbc7281a6f163581b9f77
Sasu Korhonen <smage90@hotmail.com>
2022-03-01 08:53:31 +0200

    feat: create tables to init.sql

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,14 +1,36 @@
-CREATE TABLE character
-    (
-        id integer,
-        alias varchar,
-        gender varchar,
-        name varchar,
-        photourl varchar
+create table character
+(
+    id       serial
+        primary key,
+    alias    varchar not null,
+    gender   varchar not null,
+    name     varchar not null,
+    photourl varchar not null
 );
 
 
-INSERT INTO character
+create table franchise
+(
+    id          serial
+        primary key,
+    description varchar(255),
+    name        varchar(255)
+);
+
+create table movie
+(
+    id         serial
+        primary key,
+    director   varchar(255),
+    genre      varchar(255),
+    posterurl  varchar(255),
+    title      varchar(255),
+    trailerurl varchar(255),
+    year       integer
+);
+
+
+insert into character
     (alias, gender, name, photourl) values
            ('Will', 'Male', 'Will Smith', 'asdasd'),
            ('Bruce', 'Male', 'Bruce Willis', 'asdasd');
