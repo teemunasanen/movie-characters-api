4ca165c9cac5529c5230d3b1a46100e5cedf9386
Teemu Nasanen <teemu.nasanen@gmail.com>
2022-03-01 10:25:48 +0200

    feat(controllers): added getAll, CrossOrigin and mapping

--- src/main/java/kornas/moviecharactersapi/Controllers/MovieController.java
+++ src/main/java/kornas/moviecharactersapi/Controllers/MovieController.java
@@ -3,29 +3,38 @@ package kornas.moviecharactersapi.Controllers;
 import kornas.moviecharactersapi.Models.Movie;
 import kornas.moviecharactersapi.Repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     private MovieRepository movieRepository;
 
     //create
-    @PostMapping("/movie")
-    public Movie createMovie(@RequestBody Movie movie)
-    {
+    @PostMapping("/")
+    public Movie createMovie(@RequestBody Movie movie) {
         movie = movieRepository.save(movie);
         return movie;
     }
 
+    // Get All
+    @GetMapping(value = "/")
+    public List<Movie> getMovies() {
+        return movieRepository.findAll();
+    }
+
     //Read
-    @GetMapping("/movie/{id}")
-    public Movie getMovie(@PathVariable Integer id)
-    {
+    @GetMapping("/{id}")
+    public Movie getMovie(@PathVariable Integer id) {
         Movie movie = null;
-        if(movieRepository.existsById(id))
-        {
+        if (movieRepository.existsById(id)) {
             // find will guarantee to find a unique movie
             movie = movieRepository.findById(id).get();
         }
