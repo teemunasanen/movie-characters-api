578092c8a8300faf9646dc123239a62621405849
Sasu Korhonen <smage90@hotmail.com>
2022-03-01 09:12:55 +0200

    feat: some initial data added

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -2,10 +2,10 @@ create table character
 (
     id       serial
         primary key,
-    alias    varchar not null,
-    gender   varchar not null,
-    name     varchar not null,
-    photourl varchar not null
+    alias    varchar(255) not null,
+    gender   varchar(255) not null,
+    name     varchar(255) not null,
+    photourl varchar(255) not null
 );
 
 
@@ -13,20 +13,20 @@ create table franchise
 (
     id          serial
         primary key,
-    description varchar(255),
-    name        varchar(255)
+    description varchar(255) not null,
+    name        varchar(255) not null
 );
 
 create table movie
 (
     id         serial
         primary key,
-    director   varchar(255),
-    genre      varchar(255),
+    director   varchar(255) not null,
+    genre      varchar(255) not null,
     posterurl  varchar(255),
-    title      varchar(255),
+    title      varchar(255) not null,
     trailerurl varchar(255),
-    year       integer
+    year       integer not null
 );
 
 
@@ -34,3 +34,13 @@ insert into character
     (alias, gender, name, photourl) values
            ('Will', 'Male', 'Will Smith', 'asdasd'),
            ('Bruce', 'Male', 'Bruce Willis', 'asdasd');
+
+insert into franchise
+(description, name) values
+('star wars saga', 'Star Wars Saga'),
+                           ('lord of the rings trilogy', 'Lord of The Rings Trilogy');
+
+insert into movie
+(director, genre, posterurl, title, trailerurl, "year") values
+('Ridley Scott', 'Action', null, 'Gladiator', null, 2000)
+
